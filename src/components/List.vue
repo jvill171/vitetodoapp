5e51a90df704ee5d81497c560cbb8baa6d3688d1
jvill171 <juanvillarreal64@yahoo.com>
2021-10-27 22:01:44 -0500

    Refactor Done

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import useTodos from '../composables/useTodos';
+    
+    const{pending,completed,changeStatus} = useTodos();
+
+    defineProps({
+        isCompleted: {
+            default: false,
+            type: Boolean
+        },
+    });
+</script>
+
+<template>
+    <div class="w-1/3">
+        <h3 class="text-2xl font-bold text-center text-red-500" :class="isCompleted ? 'text-green-600' : 'text-red-500'">
+            {{isCompleted ? 'Completed' : 'Pending'}}
+        </h3>
+        <ul>
+            <li v-for="todo in (isCompleted ? completed : pending)"
+            :key="todo.id"
+            @click="changeStatus(todo.id)"
+            class="w-full py-2 my-2 text-center font-bold text-black bg-gray-300 border border-black rounded-lg hover:text-white duration-500" :class="isCompleted ? 'hover:bg-red-500' : 'hover:bg-green-500'">
+            {{todo.content}}
+            </li>
+        </ul>
+    </div>
+</template>
